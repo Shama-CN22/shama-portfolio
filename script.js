0277bf5faf2eff50edcf475c896f77a1973e45ee
Shama-CN22 <shamacn537@gmail.com>
2026-03-22 20:18:30 +0530

    add portfoilo files

---
+++ script.js
@@ -0,0 +1,11 @@
+const text="Aspiring Web Developer";
+let i=0;
+
+function typing(){
+if(i<text.length){
+document.getElementById("typing").innerHTML+=text[i];
+i++;
+setTimeout(typing,100);
+}
+}
+window.onload=typing;
